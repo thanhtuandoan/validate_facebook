a29beb83ed9411719dbfcead99ea3bb816978ee1
thanhtuandoan <thanhtuandoan69@gmail.com>
2022-12-09 10:13:52 +0700

    changes validate

--- js/validate.js
+++ js/validate.js
@@ -1,8 +1,8 @@
-var $ = function(id){
+var $ = function (id) {
     return document.getElementById(id);
 }
 
-var check_valid = function(){
+var check_valid = function () {
     var is_ok = true;
     var fname = $("fname").value;
     var lname = $("lname").value;
@@ -12,59 +12,59 @@ var check_valid = function(){
     var nam = $("nam").checked;
     var nu = $("nu").checked;
     //check name
-    if(fname === ""){
+    if (fname === "") {
         is_ok = false;
         $("name_error").innerHTML = "this field is required";
-    } else{
+    } else {
         $("name_error").innerHTML = "";
     }
 
-    if(lname === ""){
+    if (lname === "") {
         is_ok = false;
         $("name_error").innerHTML = "this field is required";
-    } else{
+    } else {
         $("name_error").innerHTML = "";
     }
 
 
     //check mail
-    if(email === ""){
+    if (email === "") {
         is_ok = false;
         $("email_error").innerHTML = "this field is required";
-    } else{
+    } else {
         $("email_error").innerHTML = "";
     }
 
-    if(email != re_enter){
+    if (email != re_enter) {
         is_ok = false;
         $("re_enter_error").innerHTML = "this entry must equal first ontry";
-    }else{
-        $("re_enter_error").innerHTML = "";     
+    } else {
+        $("re_enter_error").innerHTML = "";
     }
 
     //check pass
-    if(pass === ""){
+    if (pass === "") {
         is_ok = false;
         $("pass_error").innerHTML = "this field is required";
-    } else{
+    } else {
         $("pass_error").innerHTML = "";
     }
 
     //check gender
-    if(nam == false && nu == false){
+    if (nam == false && nu == false) {
         is_ok = false;
         $("gender_error").innerHTML = "this field is required";
-    } else{
+    } else {
         $("gender_error").innerHTML = "";
     }
 
     //everything is ok
-    if(is_ok){
-        alert("Dang nhap thanh cong");
+    if (is_ok) {
+        alert("Dang ky thanh cong");
         $("register").submit();
-    }   
+    }
 }
 
-window.onload = function(){
+window.onload = function () {
     $("join").onclick = check_valid;
 }
